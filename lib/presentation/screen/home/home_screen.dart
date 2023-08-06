54b34123ea58b7812955d3a6beac46399882373e
Juan Diego G <juan.gil.trujillo@correounivalle.edu.co>
2023-08-06 16:14:00 -0500

    HomeScreen implemented

--- lib/presentation/screen/home/home_screen.dart
+++ lib/presentation/screen/home/home_screen.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:wigets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,6 +12,47 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar( 
         title: const Text("Home screen + Material3"), 
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index){
+        final menuItem = appMenuItems[index];
+        return _CustomListTile(menuItem: menuItem);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({ 
+    required this.menuItem,
+  });
+
+  final MenuItems menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of( context ).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary,),
+      trailing: Icon( Icons.arrow_forward_ios_outlined, color: colors.primary,),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap: () {
+        // TODO: Navegar a otras pantallas
+      },
     );
   }
 }
